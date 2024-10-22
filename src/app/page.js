6a1495fc3660551431cf78c41592e04f11d53702
SamaraFellaDina <sammybensink@gmail.com>
2024-10-22 15:46:16 +0200

    toevoegen van .env

--- src/app/page.js
+++ src/app/page.js
@@ -2,31 +2,26 @@ import fetchJson from "@/lib/fetch-json";
 import styles from "./page.module.css";
 import ErrorResult from '../lib/ErrorResult/ErrorResult';
 import AccesibilityGraph from '../lib/AccesibilityGraph/AccesibilityGraph'
-import API from './API/route'
 
+export default async function Home() {
+  const API = process.env.NEXT_PUBLIC_API_URL;
+  const urlSites = `${API}/frd_site`;
+  const urlScans = `${API}/frd_scans`;
 
-export default async function Home({children}) {
-  let API = await 'https://fdnd-agency.directus.app/items'
-  let urlSites = `${API}/frd_site`
-  let urlScans = `${API}/frd_scans`;
+  const sites = await fetchJson(urlSites);
+  const scans = await fetchJson(urlScans);
 
-  let sites = await fetchJson(urlSites);
-  let scans = await fetchJson(urlScans);
+  const object = 0;
+  const sitesData = sites.data[object];
+  const scansData = scans.data[object];
 
-  let object = 0
-  let sitesData = sites.data[object]
-  let scansData = scans.data[object]
-
-  console.log(sites.data[object])
   return (
     <main>
-    <ErrorResult />
-    <AccesibilityGraph />
-    
-      <h1>Test</h1>
+      <ErrorResult />
+      <AccesibilityGraph />
+      <h1>{scansData.title}</h1>
     </main>
   );
 }
-
 
 
