6dcb3f150ecbb82e0b4eea1900ec1100242da990
SamaraFellaDina <sammybensink@gmail.com>
2024-10-22 14:57:16 +0200

    proberen op te halen van data #60

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import ErrorResult from '../lib/ErrorResult/ErrorResult';
 import AccesibilityGraph from '../lib/AccesibilityGraph/AccesibilityGraph'
+import API from './API/route'
 
 
 export default function Home({children,}) {
