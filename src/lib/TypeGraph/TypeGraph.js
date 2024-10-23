3fc4ef2f96bbc95bfc0a8fbdc7d683073e9f9104
SamaraFellaDina <sammybensink@gmail.com>
2024-10-23 21:38:30 +0200

    updaten van graph SamaraFellaDina/choices-choices-the-tech-stack#45

--- src/lib/TypeGraph/TypeGraph.js
+++ src/lib/TypeGraph/TypeGraph.js
@@ -1,6 +1,6 @@
 "use client"
 
-import {React} from 'react';
+import {useEffect, useState} from 'react';
 import styles from './TypeGraph.module.css';
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, BarElement, Title, Tooltip, Legend, Filler, } from "chart.js";
 import { Bar } from "react-chartjs-2"
@@ -8,149 +8,131 @@ import { Bar } from "react-chartjs-2"
 ChartJS.register(CategoryScale, LinearScale, PointElement, BarElement, Title, Tooltip, Legend, Filler );
 
 
-const TypeGraph = ({scansData}) => {
+const TypeGraph = ({ scansData }) => {
+  const results = scansData.result;
 
-  let results = scansData.result;
+  const ondekteFouten = 0;
+  const contrastFouten = 1;
+  const structueleFouten = 2;
+  const ontbrekendeAltTeksten = 3;
+  const ontbrekendeAriaLabels = 4; 
 
-  let ondekteFouten = 0;
-  let contrastFouten = 1;
-  let structueleFouten = 2;
-  let ontbrekendeAltTeksten= 3;
-  let OnbrekendeAriaLabels = 4; 
+  useEffect(() => {
+    let rootStyles = getComputedStyle(document.documentElement);
+    let colorBlue = rootStyles.getPropertyValue("--color-blue");
+    let colorLightBlue = rootStyles.getPropertyValue("--color-lightblue");
+    let fontFamily = rootStyles.getPropertyValue("--font-family");
+    let colorBlack = rootStyles.getPropertyValue("--color-black");
 
-  const rootStyles = getComputedStyle(document.documentElement);
-  const colorBlue = rootStyles.getPropertyValue("--color-blue");
-  const colorLightBlue = rootStyles.getPropertyValue("--color-lightblue");
-  const fontFamily = rootStyles.getPropertyValue("--font-family");
-  const colorBlack = rootStyles.getPropertyValue("--color-black");
+    const labels = [
+      results[ondekteFouten].title, 
+      results[contrastFouten].title,
+      results[structueleFouten].title,
+      results[ontbrekendeAltTeksten].title,
+      results[ontbrekendeAriaLabels].title
+    ];
+    const datasets = [
+      results[ondekteFouten].amount, 
+      results[contrastFouten].amount,
+      results[structueleFouten].amount,
+      results[ontbrekendeAltTeksten].amount,
+      results[ontbrekendeAriaLabels].amount
+    ];
 
-  console.log(results)
-  const labels = [
-    results[ondekteFouten].title, 
-    results[contrastFouten].title,
-    results[structueleFouten].title,
-    results[ontbrekendeAltTeksten].title,
-    results[ontbrekendeAltTeksten].title
-  ];
-  const datasets = [
-    results[ondekteFouten].amount, 
-    results[contrastFouten].amount,
-    results[structueleFouten].amount,
-    results[ontbrekendeAltTeksten].amount,
-    results[ontbrekendeAltTeksten].amount
-  ];
-  const data = {
-    labels: labels,
-    datasets: [
-      {
-        label: "Fouten",
-        data: datasets,
-        borderColor: colorBlue,
-        backgroundColor: colorLightBlue,
-        borderWidth: 3,
-      },
-    ],
-  };
-  
-  const options = {
-    scales: {
-      y: {
-        beginAtZero: true,
-        title: {
-          display: false,
-          text: "Aantal",
+    // Set chart data
+    setChartData({
+      labels: labels,
+      datasets: [
+        {
+          label: "Fouten",
+          data: datasets,
+          borderColor: colorBlue,
+          backgroundColor: colorLightBlue,
+          borderWidth: 3,
         },
-        grid: {
-          display: false,
-        },
-        ticks: {
-          font: {
-            family: fontFamily,
-            size: 16,
-          },
-        }
-      },
-      x: {
-        title: {
-          display: false,
-          text: "Type fouten",
-        },
-        grid: {
-          display: false,
-        },
-        ticks: {
-          font: {
-            family: fontFamily,
-            size: 16,
-          },
-        }
-      },
-    },
-    plugins: {
-      legend: {
-        display: false,
-      },
-      datalabels: {
-        color: colorBlack,
-        anchor: "center",
-        align: "center",
-        font: {
-          family: fontFamily,
-          weight: "bold",
-          size: 18,
-        },
-        formatter: (value) => value,
-      },
-      tooltip: {
-        enabled: true, // Enable tooltips
-        titleAlign: "center",
-        bodyAlign: "center",
-        backgroundColor: "#FFFFFF",
-        borderWidth: 2,
-        borderColor: colorBlue,
-        padding: 10,
-        displayColors: false,
-        titleFont: {
-          size: 18,
-          weight: "bold",
-          family: fontFamily,
-        },
-        titleColor: colorBlue,
-        bodyFont: {
-          size: 16,
-          weight: "normal",
-          family: fontFamily,
-        },
-        bodyColor: colorBlue,
-      },
-    },
-    responsive: true,
-    maintainAspectRatio: false,
-    onHover: (event, elements) => {
-      const canvas = event.native.target;
-      if (elements.length) {
-        canvas.style.cursor = "pointer";
-      } else {
-        canvas.style.cursor = "default";
-      }
-    },
-  };
-
-  return ( 
-    <section>
-    <div style={{ width: "500px" }}>
-      <Bar data={data} options={options} />
-    </div> 
-    <h2>Soorten foutmeldingen</h2>
-    <figure>
-      <canvas id="bar-chart" aria-label="Soorten fouten grafiek">
-      <ul>
+      ],
+    });
     
-      </ul>
+    // Set chart options
+    setChartOptions({
+      scales: {
+        y: {
+          beginAtZero: true,
+          grid: {
+            display: false,
+          },
+          ticks: {
+            font: {
+              family: fontFamily,
+              size: 16,
+            },
+          },
+        },
+        x: {
+          grid: {
+            display: false,
+          },
+          ticks: {
+            font: {
+              family: fontFamily,
+              size: 16,
+            },
+          },
+        },
+      },
+      plugins: {
+        legend: {
+          display: false,
+        },
+        tooltip: {
+          enabled: true,
+          titleAlign: "center",
+          bodyAlign: "center",
+          backgroundColor: "#FFFFFF",
+          borderWidth: 2,
+          borderColor: colorBlue,
+          padding: 10,
+          displayColors: false,
+          titleFont: {
+            size: 18,
+            weight: "bold",
+            family: fontFamily,
+          },
+          titleColor: colorBlue,
+          bodyFont: {
+            size: 16,
+            weight: "normal",
+            family: fontFamily,
+          },
+          bodyColor: colorBlue,
+        },
+      },
+      responsive: true,
+      maintainAspectRatio: false,
+      onHover: (event, elements) => {
+        const canvas = event.native.target;
+        if (elements.length) {
+          canvas.style.cursor = "pointer";
+        } else {
+          canvas.style.cursor = "default";
+        }
+      },
+    });
+  }, [results]); // Add results to the dependency array
 
-      </canvas>
-    </figure>
-  </section>
+  return (
+    <section className={styles.section}>
+      <h2>Soorten foutmeldingen</h2>
+      <figure>
+        <canvas id="bar-chart" aria-label="Soorten fouten grafiek">
+          <ul>
+            <li>
+              <Bar data={chartData} options={chartOptions} />
+            </li>
+          </ul>
+        </canvas>
+      </figure>
+    </section>
   );
 };
 
