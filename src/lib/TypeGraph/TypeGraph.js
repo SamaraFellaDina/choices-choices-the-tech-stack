bfa369dbbb14493f6a892592de433cf391515751
SamaraFellaDina <sammybensink@gmail.com>
2024-10-23 17:39:22 +0200

    stijling aanbrengen aan chart #45

--- src/lib/TypeGraph/TypeGraph.js
+++ src/lib/TypeGraph/TypeGraph.js
@@ -1,6 +1,6 @@
 "use client"
 
-import React from 'react';
+import {React} from 'react';
 import styles from './TypeGraph.module.css';
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, BarElement, Title, Tooltip, Legend, Filler, } from "chart.js";
 import { Bar } from "react-chartjs-2"
@@ -17,6 +17,12 @@ const TypeGraph = ({scansData}) => {
   let structueleFouten = 2;
   let ontbrekendeAltTeksten= 3;
   let OnbrekendeAriaLabels = 4; 
+
+  const rootStyles = getComputedStyle(document.documentElement);
+  const colorBlue = rootStyles.getPropertyValue("--color-blue");
+  const colorLightBlue = rootStyles.getPropertyValue("--color-lightblue");
+  const fontFamily = rootStyles.getPropertyValue("--font-family");
+  const colorBlack = rootStyles.getPropertyValue("--color-black");
 
   console.log(results)
   const labels = [
@@ -37,55 +43,102 @@ const TypeGraph = ({scansData}) => {
     labels: labels,
     datasets: [
       {
-        // Title of Graph
-        label: "My Bar Chart",
+        label: "Fouten",
         data: datasets,
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-          "rgba(255, 205, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-        ],
-        borderColor: [
-          "rgb(255, 99, 132)",
-          "rgb(255, 159, 64)",
-          "rgb(255, 205, 86)",
-          "rgb(75, 192, 192)",
-        ],
-        borderWidth: 1,
-        barPercentage: 1,
-        borderRadius: {
-          topLeft: 5,
-          topRight: 5,
-        },
+        borderColor: colorBlue,
+        backgroundColor: colorLightBlue,
+        borderWidth: 3,
       },
-      // insert similar in dataset object for making multi bar chart
     ],
   };
+  
   const options = {
     scales: {
       y: {
-        title: {
-          display: true,
-          text: "Y-axis Lable",
-        },
-        display: true,
         beginAtZero: true,
-        max: 100,
+        title: {
+          display: false,
+          text: "Aantal",
+        },
+        grid: {
+          display: false,
+        },
+        ticks: {
+          font: {
+            family: fontFamily,
+            size: 16,
+          },
+        }
       },
       x: {
         title: {
-          display: true,
-          text: "x-axis Lable",
+          display: false,
+          text: "Type fouten",
         },
-        display: true,
+        grid: {
+          display: false,
+        },
+        ticks: {
+          font: {
+            family: fontFamily,
+            size: 16,
+          },
+        }
       },
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },
+      datalabels: {
+        color: colorBlack,
+        anchor: "center",
+        align: "center",
+        font: {
+          family: fontFamily,
+          weight: "bold",
+          size: 18,
+        },
+        formatter: (value) => value,
+      },
+      tooltip: {
+        enabled: true, // Enable tooltips
+        titleAlign: "center",
+        bodyAlign: "center",
+        backgroundColor: "#FFFFFF",
+        borderWidth: 2,
+        borderColor: colorBlue,
+        padding: 10,
+        displayColors: false,
+        titleFont: {
+          size: 18,
+          weight: "bold",
+          family: fontFamily,
+        },
+        titleColor: colorBlue,
+        bodyFont: {
+          size: 16,
+          weight: "normal",
+          family: fontFamily,
+        },
+        bodyColor: colorBlue,
+      },
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+    onHover: (event, elements) => {
+      const canvas = event.native.target;
+      if (elements.length) {
+        canvas.style.cursor = "pointer";
+      } else {
+        canvas.style.cursor = "default";
+      }
     },
   };
 
   return ( 
     <section>
-    <div style={{ width: "1000px" }}>
+    <div style={{ width: "500px" }}>
       <Bar data={data} options={options} />
     </div> 
     <h2>Soorten foutmeldingen</h2>
