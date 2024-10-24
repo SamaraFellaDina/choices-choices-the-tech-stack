0a7084d50d6229af2db1161f80c8798b59f95f94
SamaraFellaDina <sammybensink@gmail.com>
2024-10-24 18:15:23 +0200

    Stijling van componenten en icons importeren #48 #45

--- src/lib/Title/Title.js
+++ src/lib/Title/Title.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Title.module.css'
+import Icon from '@/lib/icon'
 
 const Title = ({sitesData}) => {
   return (
@@ -8,11 +9,11 @@ const Title = ({sitesData}) => {
     <h1 className={styles.h1}>{sitesData.title}/home</h1>
     <p>Automatische Scan</p>
   </div>
-  {/* <nav>
-    <button><Icon name="arrow-left" /></button>
-     <span>Aug.</span>
-    <button><Icon name="arrow-right" /></button>
-  </nav> */}
+  <nav className={styles.nav}>
+    <button className={styles.button}><Icon name="arrow-left" /></button>
+     <span className={styles.span}>Aug.</span>
+    <button className={styles.button}><Icon name="arrow-right" /></button>
+  </nav>
 </section>
   );
 };
